004777d682c83213b4a07eb53aa640c64e7c3fad
Priya Ranjan Padhan <priyaranjanpadhan33@gmail.com>
2026-07-30 16:45:17 +0530

    updating gitignore file

--- backend/Database/index.js
+++ backend/Database/index.js
@@ -1,9 +1,15 @@
-import pg from "pg";
+import pg, { Pool } from "pg";
 import env from "dotenv";
 
 env.config();//for working of the process.env. we need this.
 // console.log(process.env.PG_PASSWORD);
 //basic database connection
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl:{
+        rejectUnauthorized: false
+    }
+});
 const db = new pg.Client({
     user: process.env.PG_USER,
     host: process.env.PG_HOST,
--- backend/hash.js
+++
@@ -1,8 +0,0 @@
-import bcrypt from "bcrypt";
-
-async function generateHash(){
-    const hash = await bcrypt.hash("password123", 10);
-    console.log("hash:", hash);
-}
-
-generateHash();
