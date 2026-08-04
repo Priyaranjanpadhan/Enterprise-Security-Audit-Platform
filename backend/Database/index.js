c6cb8f29f035abbaed0b33cd2daa7fa73253cb70
Priya Ranjan Padhan <priyaranjanpadhan33@gmail.com>
2026-08-05 00:05:41 +0530

    Fix database export part

--- backend/Database/index.js
+++ backend/Database/index.js
@@ -4,19 +4,21 @@ import env from "dotenv";
 env.config();//for working of the process.env. we need this.
 // console.log(process.env.PG_PASSWORD);
 //basic database connection
+// We use Pool and assign it to 'db' so we don't have to change 
+// the db.query() commands in all your other router files!
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl:{
         rejectUnauthorized: false
     }
 });
-const db = new pg.Client({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: 5432
-});
-db.connect();
+// const db = new pg.Client({
+//     user: process.env.PG_USER,
+//     host: process.env.PG_HOST,
+//     database: process.env.PG_DATABASE,
+//     password: process.env.PG_PASSWORD,
+//     port: 5432
+// });
+// db.connect();
 
 export default db;
